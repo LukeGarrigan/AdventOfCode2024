e69bccc1638ffaaa4853bc5ab4a53b47cc872103
Luke <lukegarrigan8@gmail.com>
2024-12-25 06:08:58 +0000

    day 25 p1

---
+++ AdventOfCode2024/Day25/DayTwentyFiveSolver.cs
@@ -0,0 +1,72 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+
+namespace AdventOfCode2024.Day25;
+
+public class DayTwentyFiveSolver : ISolver
+{
+    public string PartOne(string[] input)
+    {
+        var keys = new List<string>();
+        var locks = new List<string>();
+
+        var count = 0;
+        var isLock = input[0] == "#####";
+        var current = "";
+        for (var i = 0; i < input.Length; i++)
+        {
+            if (input[i] == "")
+            {
+                if (isLock)
+                {
+                    locks.Add(current);
+                }
+                else
+                {
+                    keys.Add(current);
+                }
+
+                count++;
+                isLock = input[i + 1] == "#####";
+                current = "";
+                continue;
+            }
+
+            current += input[i];
+        }
+
+        if (isLock)
+        {
+            locks.Add(current);
+        }
+        else
+        {
+            keys.Add(current);
+        }
+
+        var pairs = 0;
+
+        foreach (var currentLock in locks)
+        {
+            foreach (var key in keys)
+            {
+                var isMatch = true;
+                for (var i = 0; i < currentLock.Length; i++)
+                {
+                    if (currentLock[i] == '#' &&  key[i] == '#')
+                    {
+                        isMatch = false;
+                    }
+                }
+
+                if (isMatch) pairs++;
+            }
+        }
+
+        return pairs.ToString();
+    }
+
+    public string PartTwo(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+}
--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -1,7 +1,7 @@
-﻿using AdventOfCode2024.Day24;
+﻿using AdventOfCode2024.Day25;
 
-var lines = File.ReadAllLines("Day24/input.txt");
+var lines = File.ReadAllLines("Day25/input.txt");
 
-var solver = new DayTwentyFourSolver();
+var solver = new DayTwentyFiveSolver();
 Console.WriteLine(solver.PartOne(lines));
 Console.WriteLine(solver.PartTwo(lines));
